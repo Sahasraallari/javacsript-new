ed731764270012d0fab4079a0de52a6d08721910
Sahasraallari <mohan.velagala@gmail.com>
2025-07-04 17:39:18 +0530

    blackpink

---
+++ script.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    var a = 4;
+    document.getElementById("ariel").innerHTML = a*a;
+}
+
+
+function demons() {
+    var b = 5;
+    document.getElementById("sahasra").innerHTML = b+b;
+}
+
+
+function slayer() {
+    var c = 6;
+    document.getElementById("flower").innerHTML = c-c;
+}
+
+
+function nezuko() {
+    var d = 7;
+    document.getElementById("lifestyle").innerHTML = d/d;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
